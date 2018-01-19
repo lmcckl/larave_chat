1d49048d7411bf21a9f13b2e1beecc3d35672326
lamcc <lamcheechoong@gmail.com>
2018-01-19 09:53:35 +0800

    touch up

---
+++ public/js/app.js
@@ -0,0 +1,38 @@
+$(function () {
+        var socket = io.connect('http://52.220.113.183:8899');
+        
+        var token = $("#token").val();
+        var user = $("#user").val();
+
+        $(document).keypress(function(e) {
+            if(e.which == 13) {
+                event.preventDefault();
+                $('#send').trigger('click');
+            }
+        });
+
+        //save the chat message
+        $('#send').click(function(e) {
+            e.preventDefault();
+            msg = $("#msg").val();
+            $.ajax({
+                type: "POST",
+                url: '/chat',
+                dataType: "json",
+                data: {'_token':token,'msg':msg,'user':user},
+                success:function(data) {
+                    console.log(data);
+                }
+            });
+            
+            //broacast the message
+            var obj = {'msg':$('#msg').val(),'user':user} 
+            socket.emit('message', obj);
+            $('#msg').val('');
+            return false;
+        });
+
+        socket.on('message', function(data){
+            $('#messages').append("<strong>" + data.user + "</strong><p>" + data.msg + "</p>");
+        });
+    });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-md-12">
-                            <div id="messages" style="height:300px;overflow:scroll;"></div>
+                            <div id="messages" class="mbox"></div>
                         </div>
                         <div class="col-md-12">
                             <form action="chat" method="post">
@@ -30,44 +30,5 @@
     </div>
 </div>
 
-<script>
-     $(function () {
-        var socket = io.connect('http://localhost:8899');
-        
-        var token = $("#token").val();
-        var user = $("#user").val();
-
-        $(document).bind('keypress', function(e) {
-            if(e.keyCode==13){
-                 $('#send').trigger('click');
-             }
-        });
-
-        //save the chat message
-        $('#send').click(function(e) {
-            e.preventDefault();
-            var msg = $("#msg").val();
-            $.ajax({
-                type: "POST",
-                url: '/chat',
-                dataType: "json",
-                data: {'_token':token,'msg':msg,'user':user},
-                success:function(data) {
-                    console.log(data);
-                }
-            });
-            
-
-            //broacast the message
-            var obj = {'msg':$('#msg').val(),'user':user} 
-            socket.emit('message', obj);
-            $('#msg').val('');
-            return false;
-        });
-
-        socket.on('message', function(data){
-            $('#messages').append("<strong>" + data.user + "</strong><p>" + data.msg + "</p>");
-        });
-    });
-</script>
+<script src="js/app.js"></script>
 @endsection
